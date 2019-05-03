b09853936f4c591515d5ae9755d3c8c26f8782ef
nashipae <nwariara@safaricomdigitalacademy.co.ke>
2019-05-03 16:32:16 +0300

    add date count pipe.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,8 @@ import { Quote } from "../quote";
 })
 export class QuoteComponent implements OnInit {
 
-  quotes = [new Quote(1, 'John Wooden', 'Things turn out best for the people who make the best of the way things turn out.',  new Date(2019,5,3)),]
+  quotes = []
+  // [new Quote(1, 'John Wooden', 'Things turn out best for the people who make the best of the way things turn out.',  new Date(2019,5,3)),]
 
   // Adding a new quote to the list.
   addNewQuote(quote){
