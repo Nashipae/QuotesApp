a279351357fea642cf4be505f394d28476455750
nashipae <nwariara@safaricomdigitalacademy.co.ke>
2019-05-03 15:58:20 +0300

    amend form fields.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -13,6 +13,7 @@ export class QuoteFormComponent implements OnInit {
   submitQuote(){
     this.addQuote.emit(this.newQuote);
   }
+
   constructor() { }
 
   ngOnInit() {
